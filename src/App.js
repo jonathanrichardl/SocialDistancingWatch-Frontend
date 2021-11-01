3e57e583f83cbddfe0706b4c80da6efc11e981e3
Aaron Tanjaya <aaront.tjy@gmail.com>
2021-11-01 16:02:05 +0700

    added menu button functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import Navbar from './components/navbar/NavigationBar.js';
+import Drawer from '@mui/material/Drawer';
+import {useState} from 'react';
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar openMenu={setMenuOpen}/>
+      <Drawer anchor="left" open={isMenuOpen} onClose={()=> setMenuOpen(false)}>
+        Menus here
+      </Drawer>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,18 +1,22 @@
 import {AppBar, Toolbar, IconButton} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
+import Typography from '@mui/material/Typography';
 
-function Navbar(){
- 
+function Navbar({openMenu}){
+    
     return(
         <AppBar position="static">
             <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu">
+                <IconButton edge="start" color="inherit" aria-label="menu" onClick={()=>openMenu(true)}>
                     <MenuIcon/>
                 </IconButton>
                 <IconButton edge="start" color="inherit" aria-label="menu">
                     <HomeIcon/>
                 </IconButton>
+                <Typography variant="h7" component="div">
+                    Social Distancing App
+                </Typography>
             </Toolbar>
         </AppBar>
     );
