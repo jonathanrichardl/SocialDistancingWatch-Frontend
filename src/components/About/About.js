831c1f0833e52b898d8f68c76bf5a875b9b1a7e1
karkati <karkatimustika@gmail.com>
2021-11-14 19:07:01 +0700

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,21 @@
 
 import Typography from '@mui/material/Typography';
-function About(){
+import { createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
-    return(
-
-        <div>
-            <Typography variant ="h5" component="p">
-                About Project
-            </Typography>
-
-            <Typography variant="body1" component="p">
-                indikasi pelanggaran terjadi saat jarak antara orang kurang dari 1.5 menter sesuai dengan KEMENKES PADK
-            </Typography>
-        </div>
-
+function About() {
+    return (
+      <Typography component="div">
+        <Box sx={{ lineHeight: 'normal', m: 1 , fontSize: 'h4.fontSize', fontWeight: 'bold'}}>About us.</Box>
+        <Box sx={{ lineHeight: 2, m: 1 ,fontSize: 'h6.fontSize', fontWeight: 'bold', fontStyle: 'italic'}}> Social Distancing App</Box>
+        <Box sx={{ lineHeight: 2, m: 1 ,fontSize: 'body1.fontSize'}}>Sistem Pelanggaran Social Distancing pada Lingkungan Pendidikan</Box>
+        <Box sx={{  m: 1 ,fontSize: 'body1.fontSize'}}> Aplikasi ini menyediakan informasi terjadinya indikasi pelanggaran 
+         saat jarak antara orang kurang dari 1.5 meter sesuai dengan Peraturan KEMENKES PADK untuk memenuhi penyelenggaraan Pembelajaran Tatap Muka.</Box>
+        <Box sx={{ lineHeight: 2, m: 1 , fontSize: 'h5.fontSize', fontWeight: 'bold'}}>Developed By :</Box>
+        <Box sx={{ lineHeight: 1, m: 2 , fontSize: 'body1.fontSize'}}>Team RasberryPi - Desain Proyek 1.5</Box>
+   
+      </Typography>
     );
-}
+  }
 
-export default About;
+  export default About;
