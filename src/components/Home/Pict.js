4b9a04ff7e8a0e6d197523cb72f3cc668648aa7b
aarontanjaya <aaront.tjy@gmail.com>
2021-11-16 23:00:51 +0700

    added buttons

--- src/components/Home/Pict.js
+++ src/components/Home/Pict.js
@@ -1,22 +1,32 @@
-import {Paper, Typography} from '@mui/material';
+import {Paper, Typography, IconButton} from '@mui/material';
 import React from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import "./Pict.css"
 
 function Pict({data,timestamp}){
-
+    
     return(
-        
         <Paper elevation={2} className={'frame'}>
+
             <Typography variant="h6" component="div">
                 Gambar Pelanggaran
-                </Typography>
-            <Typography variant="body1" component="div">
-                {data[0].kelas}
             </Typography>
-            <img src={"/samoyed.jpg"} alt={'cute dog'} className={'picture'}/>
             <Typography variant="body2" component="div">
                 Last Update : {timestamp}    
             </Typography>
+            <img src={"/samoyed.jpg"} alt={'cute dog'} className={'picture'}/>
+            <div className={'buttombuttons'}>
+                <IconButton style={{display:"inline"}}>
+                    <ArrowBackIosIcon/>
+                </IconButton>
+                <Typography  variant="body1" component="div" display='inline' marginRight='auto' marginLeft='auto'>
+                    {data[0].kelas}
+                </Typography>
+                <IconButton style={{display:"inline",float:'right'}} >
+                    <ArrowForwardIosIcon/>
+                </IconButton>
+            </div>
         </Paper>
     );
 }
