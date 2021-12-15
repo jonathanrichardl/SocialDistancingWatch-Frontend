2571fc8f0832f376beb19c5bf38ad377380374a0
aarontanjaya <aaront.tjy@gmail.com>
2021-12-15 13:20:14 +0700

    add periodic load

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import MenuItem from './components/MenuItem/MenuItem.js';
 import DataDisplay from './components/DataDisplay/DataDisplay.js';
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 //testtestdoang
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 
 //spsekek
 function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [data, setData] = useState([{kelas: "Loading", violation: "Loading", timestamp: "Loading", gambar: '/samoyed.jpg'}]);
 
-  const mockdata=[{kelas: "X MIPA I", violation: 10, timestamp: "10:15", gambar: '/samoyed.jpg'},
+  /*const mockdata=[{kelas: "X MIPA I", violation: 10, timestamp: "10:15", gambar: '/samoyed.jpg'},
                   {kelas: "X MIPA II", violation: 5, timestamp: "10:20", gambar: '/contohkelas1.jpg'} ,
                   {kelas: "XI MIPA I", violation: 2, timestamp: "10:12", gambar: '/contohkelas2.jpg'},
                   {kelas: "XII MIPA I", violation: 120, timestamp: "10:12", gambar: '/contohkelas3.jpg'},
-                  {kelas: "X MIPA iI", violation: 5, timestamp: "10:12", gambar: '/contohkelas2.jpg'}];
+                  {kelas: "X MIPA iI", violation: 5, timestamp: "10:12", gambar: '/contohkelas2.jpg'}];*/
+  useEffect(() => {
+    const id = setInterval(() => 
+      fetch("http://localhost:8080/X%20IPA%1")
+        .then(function(response) {
+            if (response.status !== 200) {
+            console.log(
+              "Looks like there was a problem. Status Code: " + response.status
+            );
+            return;}
+            response.json().then(function(datanya) {
+            console.log(datanya[0]);
+            setData(datanya);
+          });
+        })
+    , 1000);
+  
+    return () => clearInterval(id);  
+  }, []);
   return (
     <Router>
       <Navbar openMenu={setMenuOpen}/>
@@ -30,11 +49,11 @@ function App() {
             <About />
           </Route>
           <Route path='/components/Home' >
-            <Home data={mockdata}/>
+            <Home data={data}/>
           </Route>
           <Route path='/components/DataDisplay' component={DataDisplay}/>
           <Route path='/'>
-            <Home data={mockdata}/>
+            <Home data={data}/>
           </Route>
       </Switch>
     </Router>
